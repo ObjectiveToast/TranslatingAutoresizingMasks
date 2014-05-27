fac07130045126c9b49abbbe63bb02bb30b5f9eb
Andrew Hershberger <andrew@twotoasters.com>
2014-05-27 09:24:27 -0400

    Initial Commit

---
+++ Translates/TWTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWTAppDelegate.h
+//  Translates
+//
+//  Created by Andrew Hershberger on 5/27/14.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Translates/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Translates
+//
+//  Created by Andrew Hershberger on 5/27/14.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWTAppDelegate class]));
+    }
+}
